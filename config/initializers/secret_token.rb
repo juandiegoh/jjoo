7a31781c40b78867fe4fcb83991845144a91918b
Juan Diego Herenu <jdherenu@gmail.com>
2013-11-25 23:15:47 -0300

    Initial Commit wit Rails Structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jjoo::Application.config.secret_token = 'ca70c975305edea1d18d8876aba2ddb40c024589b60a57cbb6fc6b3b1bd501e54973c8287728678dc1264ae004c197d10ae79b0af0ad4fa884379c4501848ed7'
